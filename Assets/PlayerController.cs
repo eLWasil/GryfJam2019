e9914f9a57af808e17cbe0bdcbb2dfe76d34216f
WhiteWolfIron <wolfmar.ww@gmail.com>
2019-05-11 04:50:54 +0200

    Rewired init

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(PlayerInput))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 50;
+    [SerializeField]
+    private float rotationSpeed = 500;
+
+    private  PlayerInput playerInput;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    private void Update()
+    {
+        Rotate();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        var move = new Vector3(playerInput.GetAxis(PlayerInput.InputActions.Horizontal), 0, playerInput.GetAxis(PlayerInput.InputActions.Vertical));
+        //transform.position += transform.InverseTransformDirection(move) * moveSpeed * Time.deltaTime;
+        transform.position += move * moveSpeed * Time.deltaTime;
+    }
+
+    void Rotate()
+    {
+        var move = new Vector3(playerInput.GetAxis(PlayerInput.InputActions.RightHorizontal), 0, playerInput.GetAxis(PlayerInput.InputActions.RightVertical));
+        if (move == Vector3.zero) return;
+
+        Quaternion wantedRotation = Quaternion.LookRotation(move);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, wantedRotation, rotationSpeed * Time.deltaTime);
+
+        //var move = new Vector3(playerInput.GetAxis(PlayerInput.InputActions.Rotation), 0, 0);
+        //var rotation = playerInput.GetAxis(PlayerInput.InputActions.Rotation);
+        //transform.Rotate(0, rotation * rotationSpeed * Time.deltaTime, 0);
+    }
+}
---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class PlayerInput : MonoBehaviour
+{
+    public enum PlayerId
+    {
+        none = 0,
+        pad_1 = 1,
+        pad_2 = 2,
+        pad_3 = 3,
+        pad_4 = 4,
+        keyboard_1 = 5,
+        keyboard_2 = 6,
+        test = 7
+    }
+    public enum InputActions
+    {
+        Vertical = 0,
+        Horizontal = 1,
+        RightVertical = 2,
+        RightHorizontal = 3,
+        Attack = 4,
+        Cancel = 5,
+        Menu = 6
+    }
+
+    public PlayerId playerId = PlayerId.none;
+
+    private Player rewiredPlayer
+    {
+        get
+        {
+            return ReInput.players.GetPlayer((int)playerId - 1);
+        }
+    }
+    public bool GetButtonDown(InputActions input)
+    {
+        return rewiredPlayer.GetButtonDown(input.ToString());
+    }
+
+    public bool GetButtonUp(InputActions input)
+    {
+        return rewiredPlayer.GetButtonUp(input.ToString());
+    }
+
+    public bool GetButton(InputActions input)
+    {
+        return rewiredPlayer.GetButton(input.ToString());
+    }
+
+    public float GetAxis(InputActions input)
+    {
+        return rewiredPlayer.GetAxis(input.ToString());
+    }
+}
