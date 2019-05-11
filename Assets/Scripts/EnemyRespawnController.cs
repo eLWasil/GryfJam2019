754a79a604e63b4c4278dace05dc77ca43f1336c
lukasz.wasilewski0395@gmail.com <lukasz.wasilewski0395@gmail.com>
2019-05-11 03:24:51 +0200

    Komituje za łukasza po polsku. Jakieś jego zmiany ale nie wiem co dokładnie bo pojechał do domu i siedzę sam :c

---
+++ Assets/Scripts/EnemyRespawnController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+
+public class EnemyRespawnController : MonoBehaviour
+{
+    public GameObject enemiePrefab;
+
+    private Stopwatch stopwatch = new Stopwatch();
+    private const int respawnTimeMiliseconds = 400;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stopwatch.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //UnityEngine.Debug.Log("stopwatch.ElapsedMilliseconds = " + stopwatch.ElapsedMilliseconds);
+
+        if (stopwatch.ElapsedMilliseconds >= respawnTimeMiliseconds)
+        {
+            bool isRespawnHorizontal = randomBoolean();
+
+            float shortRangePosition = Random.Range(40.0f, 50.0f);
+            float  longRangePosition = Random.Range(-50.0f, 50.0f);
+
+            float xPos = 0;
+            float zPos = 0;
+
+            if (isRespawnHorizontal)
+            {
+                xPos = (randomBoolean() ? shortRangePosition : shortRangePosition * -1);
+                zPos = longRangePosition;
+            }
+            else
+            {
+                zPos = (randomBoolean() ? shortRangePosition : shortRangePosition * -1);
+                xPos = longRangePosition;
+            }
+
+
+            var enemy = Instantiate(enemiePrefab, new Vector3(xPos, 0, zPos), Quaternion.identity);
+            enemy.transform.SetParent(transform);
+
+            stopwatch.Restart();
+            stopwatch.Start();
+        }
+        //Debug.Log("enemies.Count: " + enemies.Count);
+
+    }
+
+    private bool randomBoolean()
+    {
+        if (Random.value >= 0.5)
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Assets/Scripts/EnemyTrace.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTrace : MonoBehaviour
+{
+    public float speed;
+    public GameObject granary;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float granaryLeftBorder  = granary.transform.position.x - (granary.transform.localScale.x / 2);
+        float granaryRightBorder = granary.transform.position.x + (granary.transform.localScale.x / 2);
+        float granaryTopBorder   = granary.transform.position.z + (granary.transform.localScale.z / 2);
+        float granaryBotBorder   = granary.transform.position.z - (granary.transform.localScale.z / 2);
+
+        if (transform.position.x < granaryLeftBorder)
+        {
+            transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > granaryRightBorder)
+        {
+            transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.z < granaryBotBorder)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + speed);
+        }
+        else if (transform.position.z > granaryTopBorder)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - speed);
+        }
+
+        if (granaryLeftBorder < transform.position.x && granaryRightBorder > transform.position.x)
+        {
+            if (granaryBotBorder < transform.position.z && granaryTopBorder > transform.position.z)
+            {
+                Debug.Log("Destroyed");
+                Destroy(gameObject);
+            }
+        }
+
+    }
+
+
+}
